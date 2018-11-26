1624e3fa04de230d14ba7f3378f0a60b3dd3587c
jah1603 <hendersonjamesalexander@gmail.com>
2018-11-26 13:08:39 +0000

    getters and setters and collection methods

--- src/main/java/models/Pupil.java
+++ src/main/java/models/Pupil.java
@@ -1,4 +1,5 @@
-package models;
+
+import java.util.stream.Collectors;
 import java.util.Set;
 import java.util.HashSet;
 
@@ -25,4 +26,58 @@ public class Pupil {
     }
 
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Set<Pupil> getFriends(final Set<Pupil> others) {
+        return others.stream().filter(other -> friends.contains(other))
+                .collect(Collectors.toSet());
+    }
+
+    public Set<Pupil> getEnemies(final Set<Pupil> others) {
+        return others.stream().filter(other -> enemies.contains(other))
+                .collect(Collectors.toSet());
+    }
+
+    public Set<Pupil> getDisruptivePartnerships(final Set<Pupil> others) {
+        return others.stream().filter(other -> enemies.contains(other))
+                .collect(Collectors.toSet());
+    }
+
+    public Boolean hasSensoryImpairment() {
+        return sensory_impairment;
+    }
+
+    public void setSensoryImpairment(Boolean sensory_impairment) {
+        this.sensory_impairment = sensory_impairment;
+    }
+
+    public Boolean isGenerallyAttentive() {
+        return generally_attentive;
+    }
+
+    public void setGenerallyAttentive(Boolean generally_attentive) {
+        this.generally_attentive = generally_attentive;
+    }
+
+    public Boolean isAcademicAchiever() {
+        return academic_achiever;
+    }
+
+    public void setAcademicAchiever(Boolean academic_achiever) {
+        this.academic_achiever = academic_achiever;
+    }
+
+    public Desk getDesk() {
+        return desk;
+    }
+
+    public void setDesk(Desk desk) {
+        this.desk = desk;
+    }
 }
