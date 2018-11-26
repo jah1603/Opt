6e50772911b927a2d93dc436e07599d87473f446
jah1603 <hendersonjamesalexander@gmail.com>
2018-11-26 13:49:22 +0000

    pupil class errors resolved

--- src/main/java/models/Pupil.java
+++ src/main/java/models/Pupil.java
@@ -1,14 +1,15 @@
-
+import models.Desk;
 import java.util.stream.Collectors;
 import java.util.Set;
 import java.util.HashSet;
+@SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
 
 public class Pupil {
 
     private int id;
-    private final Set<Pupil> friends = new HashSet<Pupil>();
-    private final Set<Pupil> enemies = new HashSet<Pupil>();
-    private final Set<Pupil> disruptive_partnerships = new HashSet<Pupil>;
+    private Set<Pupil> friends = new HashSet<>();
+    private Set<Pupil> enemies = new HashSet<>();
+    private Set<Pupil> disruptive_partnerships = new HashSet<Pupil>();
     private Boolean sensory_impairment;
     private Boolean generally_attentive;
     private Boolean academic_achiever;
@@ -22,6 +23,8 @@ public class Pupil {
 
         this.id = id;
         this.sensory_impairment = sensory_impairment;
+        this.generally_attentive = generally_attentive;
+        this.academic_achiever = academic_achiever;
 
     }
 
@@ -35,17 +38,17 @@ public class Pupil {
     }
 
     public Set<Pupil> getFriends(final Set<Pupil> others) {
-        return others.stream().filter(other -> friends.contains(other))
+        return others.stream().filter(friends::contains)
                 .collect(Collectors.toSet());
     }
 
     public Set<Pupil> getEnemies(final Set<Pupil> others) {
-        return others.stream().filter(other -> enemies.contains(other))
+        return others.stream().filter(enemies::contains)
                 .collect(Collectors.toSet());
     }
 
     public Set<Pupil> getDisruptivePartnerships(final Set<Pupil> others) {
-        return others.stream().filter(other -> enemies.contains(other))
+        return others.stream().filter(disruptive_partnerships::contains)
                 .collect(Collectors.toSet());
     }
 
